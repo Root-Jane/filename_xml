722b6c2e3f39155253417583dc0fb33b3fbd0109
wantings <jianyujingjing@sina.cn>
2016-06-22 17:39:18 +0800

    second commit

--- filename_xml.py
+++ filename_xml.py
@@ -1,7 +1,15 @@
 import os
 import os.path
 
-path = r"C:\Users\Root\Documents\GitHub\xml"  #根目录路径
-for root, dirs, files in os.walk(path):       #os.walk遍历根目录所有文件
-    for filename in files:                    #打印出所有文件路径和名称
-        print(root + '\\' + filename)
+path = r"L:\bin"  #根目录路径
+with open(r"L:\Python\test14.xml","w",encoding="utf-8") as f:  #创建xml文件 
+	for root, dirs, files in os.walk(path):       #os.walk遍历根目录所有文件
+		for filename in files:                    #打印出所有文件路径和名称
+			filepath=root + '\\' + filename
+			filepath1=filepath.replace("\\","/").strip('L:/bin')
+			filepath1="<file path=\"" + filepath1 + "\"" + " filename=\"" + filepath1 +"\""+"/>" +"\n"
+			f.write(filepath1)
+
+	
+	
+   
