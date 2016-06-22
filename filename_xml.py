aa1fed6dcf7ad1e651f7a62d048a984216a89414
Root-lee <54rootlee@gmail.com>
2016-06-22 13:21:00 +0300

    修正格式，以及将路径与代码分开，改一些变量名称

--- filename_xml.py
+++ filename_xml.py
@@ -2,14 +2,12 @@ import os
 import os.path
 
 path = r"L:\bin"  #根目录路径
-with open(r"L:\Python\test14.xml","w",encoding="utf-8") as f:  #创建xml文件 
-	for root, dirs, files in os.walk(path):       #os.walk遍历根目录所有文件
-		for filename in files:                    #打印出所有文件路径和名称
-			filepath=root + '\\' + filename
-			filepath1=filepath.replace("\\","/").strip('L:/bin')
-			filepath1="<file path=\"" + filepath1 + "\"" + " filename=\"" + filepath1 +"\""+"/>" +"\n"
-			f.write(filepath1)
+xml_path = r"L:\Python\test14.xml"   #生成的xml文件储存路径
 
-	
-	
-   
+with open(xml_path,"w",encoding="utf-8") as f:  #创建xml文件 
+    for root, dirs, files in os.walk(path):       #os.walk遍历根目录所有文件
+        for filename in files:                    #打印出所有文件路径和名称
+            filepath=root + '\\' + filename
+            filepath=filepath.strip(path).replace("\\","/")
+            filename_xml="<file path=\"" + filepath + "\"" + " filename=\"" + filepath +"\""+"/>" +"\n"   #生成xml格式文本
+            f.write(filename_xml)
